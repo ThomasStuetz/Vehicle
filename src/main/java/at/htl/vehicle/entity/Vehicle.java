847774352b1eb189eb9e8da8d832511831c5bd27
ThomasStuetz <thomas.stuetz@gmx.at>
2015-09-05 09:16:50 +0200

    4. Ausbaustufe: REST mit Entity (inkl. JPA) CRUD für Vehicle fertiggestellt vor Umbau zu Sub Resources (inkl Umbenennung von VehicleResource zu VehicleResources)

--- src/main/java/at/htl/vehicle/entity/Vehicle.java
+++ src/main/java/at/htl/vehicle/entity/Vehicle.java
@@ -1,15 +1,22 @@
 package at.htl.vehicle.entity;
 
+import javax.persistence.*;
 import javax.xml.bind.annotation.XmlRootElement;
 
+@Entity
+@NamedQuery(name = "Vehicle.findAll", query = "SELECT v FROM Vehicle v")
 @XmlRootElement
 public class Vehicle {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String brand;
     private String type;
+    private boolean annualVignetteValid;
 
-    public Vehicle() {}
+    public Vehicle() {
+    }
 
     public Vehicle(String brand, String type) {
         this.brand = brand;
@@ -40,6 +47,14 @@ public class Vehicle {
 
     public void setType(String type) {
         this.type = type;
+    }
+
+    public boolean isAnnualVignetteValid() {
+        return annualVignetteValid;
+    }
+
+    public void setAnnualVignetteValid(boolean annualVignetteValid) {
+        this.annualVignetteValid = annualVignetteValid;
     }
     //endregion
 }
--- src/main/java/at/htl/vehicle/rest/VehicleResource.java
+++ src/main/java/at/htl/vehicle/rest/VehicleResource.java
@@ -1,36 +1,77 @@
 package at.htl.vehicle.rest;
 
+import at.htl.vehicle.business.VehicleFacade;
 import at.htl.vehicle.entity.Vehicle;
 
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.json.JsonObject;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
+@Stateless
 @Path("vehicle")
 public class VehicleResource {
+
+    @Inject
+    VehicleFacade vehicleFacade;
 
     @GET
     @Path("{id}")
     public Vehicle find(@PathParam("id") long id) {
-        return new Vehicle("Opel " + id,"Commodore");
+        return vehicleFacade.findById(id);
     }
 
     @GET
     public List<Vehicle> findAll() {
-        List<Vehicle> all = new ArrayList<>();
-        all.add(find(42));
-        return all;
+        return vehicleFacade.findAll();
     }
 
     @DELETE
     @Path("{id}")
     public void delete(@PathParam("id") long id) {
-        System.out.println("deleted = " + id);
+        vehicleFacade.delete(id);
+    }
+
+    @PUT
+    @Path("{id}")
+    public Vehicle update(@PathParam("id") long id, Vehicle vehicle) {
+        vehicle.setId(id);
+        return vehicleFacade.save(vehicle);
+    }
+
+    @PUT
+    @Path("{id}/vignette_valid")
+    public Response updateVignetteValid (@PathParam("id") long id, JsonObject vignetteValidUpdate) {
+        if (!vignetteValidUpdate.containsKey("annualVignetteValid")) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .header("reason", "JSON should contain field annualVignetteValid")
+                    .build();
+        }
+        boolean vignetteValid = vignetteValidUpdate.getBoolean("annualVignetteValid");
+        Vehicle vehicle = vehicleFacade.updateVignetteValid(id, vignetteValid);
+        if (vehicle == null) {
+            return Response
+                    .status(Response.Status.BAD_REQUEST)
+                    .header("reason", "vehicle with id " + id + " does not exist")
+                    .build();
+        } else {
+            return Response.ok(vehicle).build();
+        }
     }
 
     @POST
-    public void save(Vehicle vehicle) {
-        System.out.println("Vehicle = " + vehicle);
+    public Response save(Vehicle vehicle, @Context UriInfo info) {
+        Vehicle saved = this.vehicleFacade.save(vehicle);
+        Long id = saved.getId();
+        URI uri = info.getAbsolutePathBuilder().path("/" + id).build();
+        return Response.created(uri).build();
     }
-
 }
+
