0cfa8e7576cc7de5e84a1fe209d9f476d36ddf4b
ThomasStuetz <thomas.stuetz@gmx.at>
2015-09-05 09:54:45 +0200

    5. Ausbaustufe: REST mit Entity (inkl. JPA) Sub Resource VehicleResource erstellt

--- src/main/java/at/htl/vehicle/rest/VehicleResource.java
+++ src/main/java/at/htl/vehicle/rest/VehicleResource.java
@@ -3,52 +3,39 @@ package at.htl.vehicle.rest;
 import at.htl.vehicle.business.VehicleFacade;
 import at.htl.vehicle.entity.Vehicle;
 
-import javax.ejb.Stateless;
-import javax.inject.Inject;
 import javax.json.JsonObject;
 import javax.ws.rs.*;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriBuilder;
-import javax.ws.rs.core.UriInfo;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
 
-@Stateless
-@Path("vehicle")
 public class VehicleResource {
 
-    @Inject
+    long id;
     VehicleFacade vehicleFacade;
 
-    @GET
-    @Path("{id}")
-    public Vehicle find(@PathParam("id") long id) {
-        return vehicleFacade.findById(id);
-    }
-
-    @GET
-    public List<Vehicle> findAll() {
-        return vehicleFacade.findAll();
-    }
-
-    @DELETE
-    @Path("{id}")
-    public void delete(@PathParam("id") long id) {
-        vehicleFacade.delete(id);
+    public VehicleResource(long id, VehicleFacade vehicleFacade) {
+        this.id = id;
+        this.vehicleFacade = vehicleFacade;
     }
 
     @PUT
-    @Path("{id}")
-    public Vehicle update(@PathParam("id") long id, Vehicle vehicle) {
+    public Vehicle update(Vehicle vehicle) {
         vehicle.setId(id);
         return vehicleFacade.save(vehicle);
     }
 
+    @GET
+    public Vehicle find() {
+        return vehicleFacade.findById(id);
+    }
+
+    @DELETE
+    public void delete() {
+        vehicleFacade.delete(id);
+    }
+
     @PUT
-    @Path("{id}/vignette_valid")
-    public Response updateVignetteValid (@PathParam("id") long id, JsonObject vignetteValidUpdate) {
+    @Path("/vignette_valid")
+    public Response updateVignetteValid (JsonObject vignetteValidUpdate) {
         if (!vignetteValidUpdate.containsKey("annualVignetteValid")) {
             return Response.status(Response.Status.BAD_REQUEST)
                     .header("reason", "JSON should contain field annualVignetteValid")
@@ -65,13 +52,6 @@ public class VehicleResource {
             return Response.ok(vehicle).build();
         }
     }
-
-    @POST
-    public Response save(Vehicle vehicle, @Context UriInfo info) {
-        Vehicle saved = this.vehicleFacade.save(vehicle);
-        Long id = saved.getId();
-        URI uri = info.getAbsolutePathBuilder().path("/" + id).build();
-        return Response.created(uri).build();
-    }
 }
+
 
